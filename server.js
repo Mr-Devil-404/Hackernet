a60f63a42c9af3edc2dc5e6d5d5b165f5c31862d
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-04-27 11:39:21 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/users');
+const adminRoutes = require('./routes/admin');
+const storyRoutes = require('./routes/story');
+const app = express();
+
+dotenv.config();
+
+// Database Connection
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('MongoDB Connected Successfully!'))
+.catch((err) => console.log(err));
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/story', storyRoutes);
+
+// Server Start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Hackernet Server running on port ${PORT}`));
