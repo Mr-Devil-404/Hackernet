4d12a305ee9399019683520323f0619ac8d746a1
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-04-28 06:39:04 +0300

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
 
+const CommentSchema = new mongoose.Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        required: true,
+        max: 300
+    },
+    replies: [
+        {
+            userId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            text: {
+                type: String,
+                max: 300
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ]
+}, { timestamps: true });
+
+module.exports = mongoose.model("Comment", CommentSchema);
