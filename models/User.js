61f3b3bc093226c43f2b1e2efe498245ef2d3c82
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-04-28 10:18:14 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,28 +1,9 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
 
-const loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({ message: "User Not Found" });
-    }
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) {
-      return res.status(400).json({ message: "Invalid Password" });
-    }
-
-    res.status(200).json({
-      _id: user._id,
-      username: user.username,
-      email: user.email,
-      message: "Login Successful"
-    });
-
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
-
-module.exports = { loginUser };
+module.exports = mongoose.model('User', userSchema);
