db5ed40cfa8959a2a3f5132f39568ac9518c36b5
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-04-28 16:10:08 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/user');
+const verifyToken = require('../middleware/authMiddleware'); // Middleware Import করো
 
+// Register API
 router.post('/register', registerUser);
+
+// Login API
 router.post('/login', loginUser);
+
+// Profile API (Protected Route)
+router.get('/profile', verifyToken, (req, res) => {
+    res.status(200).json({
+        msg: "Profile Accessed Successfully!",
+        user: req.user, // Token থেকে পাওয়া ইউজার দেখাবে
+    });
+});
 
 module.exports = router;
