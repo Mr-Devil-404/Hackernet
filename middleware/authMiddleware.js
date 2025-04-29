a7a390cc41e5b250e761751dbe5fe360da485ea4
Md Mehedi <132669573+Mr-Devil-404@users.noreply.github.com>
2025-04-29 15:43:01 +0300

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,10 +4,9 @@ module.exports = function (req, res, next) {
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader.startsWith("Bearer ")) {
     const token = authHeader.split(" ")[1];
-
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded; // { id: user._id }
+      req.user = decoded;
       next();
     } catch (err) {
       return res.status(403).json({ message: "Invalid token" });
